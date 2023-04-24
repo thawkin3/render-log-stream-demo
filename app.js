9f3bf0d3f3eb99a32a02d12aea9cc065b101c2a2
Tyler Hawkins <thawkin3@byu.net>
2023-04-23 22:24:15 -0600

    feat: adds basic logs

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.static('public'))
+app.get('/', (req, res) => {
+  console.log('Log - home page');
+  console.info('Info - home page');
+  console.warn('Warn - home page');
+  console.error('Error - home page');
+  console.debug('Debug - home page');
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/index.html')));
+  return res.sendFile('index.html', { root: 'public' });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
